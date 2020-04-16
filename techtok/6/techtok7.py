d7712f6b4381fb070b7aaa046b56060ec15c2e2f
bijan <bizhan71720@gmail.com>
2020-04-16 07:45:59 +0430

    make some changes

--- techtok/6/techtok7.py
+++ techtok/6/techtok7.py
@@ -2,9 +2,9 @@ n = int(input("enter number:"))
 
 for i in range (1,n+1):
     if (i%3==1):
-        print("*",end="")
-    if (i%3==2):
-        print("#",end="")
-    if (i%3==0):
-        print("@",end="")
+        print("* ",end="")
+    elif (i%3==2):
+        print("# ",end="")
+    elif (i%3==0):
+        print("@ ",end="")
 print()
