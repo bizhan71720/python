b626912e23114caa6440d69982a2ed900f9a337d
bijan <bizhan71720@gmail.com>
2020-04-18 16:28:31 +0430

    make change to solve bog on odd numbers

--- techtok/9/techtok9.py
+++ techtok/9/techtok9.py
@@ -1,6 +1,7 @@
 n = int(input("Enter number:"))
-n = n/2
-odd=(n/2)*(2+((n-1)*2))
-even=(n/2)*(4+((n-1)*2))
+even_n=int(n/2)
+odd_n=even_n+1
+odd=(odd_n/2)*(2+((n-1)*2))
+even=(even_n/2)*(4+((n-1)*2))
 print("odd=",odd)
 print("even=",even)
