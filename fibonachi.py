6bd1b6869ba6139f329b4d0cb6dc91601c22cdf7
bijan <bizhan71720@gmail.com>
2020-05-16 23:25:56 +0430

    add color to output

--- fibonachi.py
+++ fibonachi.py
@@ -1,10 +1,15 @@
+import os
+
+class color : 
+    GREEN = '\033[92m'
+    WHITE = '\033[0m'
 n = int (input("enter number:"))
 a = 1
 b = 0 
 for i in range(1,1+n):
     s = a + b
-    print(str(i)+")")
-    print(str(s))
+    print(color.GREEN + str(i)+")")
+    print(color.WHITE + str(s))
     a = b
     b = s
     print()
