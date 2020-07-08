c28e8bc2676581cb7c17adf0d88d16657505ab66
bizhan71720 <bizhan71720@gamil.com>
2020-07-08 18:42:24 +0430

    coplet a program to check ip and writ in log file

--- ip_checker.py
+++ ip_checker.py
@@ -4,25 +4,35 @@ import datetime
 import requests
 
 ip_url="http://ifconfig.me"
+
 def ip_proxy():
-    proxies = {"http":"socks5:127.0.0.1:9050",\
-            "https":"socks5:127.0.0.1:9050"}
-    ip_proxy = requests.get(ip_url,proxies=proxies)
+    try:
+        proxies = {"http":"socks5:127.0.0.1:9050",\
+                "https":"socks5:127.0.0.1:9050"}
+        ip_proxy = requests.get(ip_url,proxies=proxies).content
+        
+    except:
+        ip_proxy = "**proxy service is not active this time*"
+
     return ip_proxy
 
 def ip_normal():
-    ip_normal = requests.get( ip_url)
+    try:
+        ip_normal = requests.get( ip_url).content
+        
+    except:
+        ip_normal = "**internet connection refused*"
     return ip_normal
 
-ip_proxy = ip_proxy().content
-ip_normal = ip_normal().content
+ip_proxy = ip_proxy()
+ip_normal = ip_normal()
 
 date = datetime.datetime.now()
-filename=str(date.year)+"-"+str(date.month)+"-"+str(date.day)
+filename=str(date.year)+"-"+str(date.month)+"-"+str(date.day)+".txt"#can add your path in first of this string
 
-ips = str(ip_normal)[2:-1]+"/\/\\"+str(ip_proxy)[2:-1]
-data = str(date.hour)+"-"+str(date.minute)+"-"+str(date.second)+"---------->"+ ips+"\n"
+ips = str(ip_normal)[2:-1]+"\t/\/\\\t"+str(ip_proxy)[2:-1]
+data = str(date.hour)+":"+str(date.minute)+"\t──────────>\t"+ ips+"\n"
 
-openfile=open(filename,"a")
-openfile.write(data)
-openfile.close()
+of = open(filename, 'a')
+of.write(data)
+of.close()
